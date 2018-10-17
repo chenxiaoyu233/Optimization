db2b56e0b55a26fb3f1419eac2a2a771482a613f
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2018-10-18 01:21:02 +0800

    修复了优化之后带来的Simple版本的bug

--- k-defective/kDefectiveSimple.cpp
+++ k-defective/kDefectiveSimple.cpp
@@ -5,11 +5,11 @@ KDefectiveSimple::KDefectiveSimple(int n): KDefectiveBase(n) { }
 void KDefectiveSimple::branch(void *P, void *C, int k, int m) {
 	for (int i = 0; i < size; i++) if (this -> existsInSet(C, i)) {
 		int need = this -> calcNeedEdge(P, C, i);
-		this -> removeVertexFromSet(C, i);
-		this -> addVertexToSet(P, i);
+		this -> removeVertexFromSetSync(C, i, 'C');
+		this -> addVertexToSetSync(P, i, 'P');
 		this -> solve(P, C, k, m - need);
 
-		this -> removeVertexFromSet(P, i);
+		this -> removeVertexFromSetSync(P, i, 'P');
 		this -> solve(P, C, k, m);
         
         break;
