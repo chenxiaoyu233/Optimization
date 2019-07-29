b9e920f4ccb28bba1ddb008dac8302c0c2b14729
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-07-29 22:08:36 +0800

    [fix bug] [update]

--- k-defective/MoMC2016.h
+++ k-defective/MoMC2016.h
@@ -1,13 +1,15 @@
 #ifndef __MoMC2016_H__
 #define __MoMC2016_H__
 
+#include "kDefectiveR2MC.h"
+
 extern KDefectiveR2MC *shell;
 void MOMCSolver();
 
 #define TIME_CUTTER do {\
     if (shell -> timeIsUp()) {\
         shell -> notFinish = true;\
-        return;\
+        return 0;\
     }\
 } while (0)
 
