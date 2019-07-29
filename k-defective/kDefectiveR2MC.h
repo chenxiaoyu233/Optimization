43f914613a16e4cba95e9b297e2e7be20ac6c245
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2019-07-29 22:28:50 +0800

    [fix bug] [update]

--- k-defective/kDefectiveR2MC.h
+++ k-defective/kDefectiveR2MC.h
@@ -9,12 +9,12 @@ class KDefectiveR2MC: public KDefectiveBase {
         vector<pair<int, int> > edges;
         list<pair<int, int> > edge_stack;
         bool *graph;
-        stringstream ss;
         void solve(int k); 
         void encodeGraph();
         void search(int idx, int k);
         void callMaximumCliqueSolver();
     public:
+        stringstream ss;
         KDefectiveR2MC(int n);
         virtual void AddEdgeByVector(const vector<pair<int, int> > &edges);
         virtual int Solve(int k);
